78103dbd2172d9e242c4af92071cbe81cd515970
Juned KH <kjuned007@gmail.com>
2022-12-09 19:32:24 +0530

    I hope this work as i want

--- update.py
+++ update.py
@@ -1,9 +1,74 @@
-from logging import error, info
-from os import path, remove
+from logging import INFO, FileHandler, StreamHandler, basicConfig, error, info
+from os import environ, path, remove
 from platform import machine
 from subprocess import run
 
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from requests import request
+
+if path.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
+        f.truncate(0)
+
+basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[FileHandler('log.txt'), StreamHandler()],
+                    level=INFO)
+
+load_dotenv('config.env', override=True)
+
+try:
+    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
+        error('The README.md file there to be read! Exiting now!')
+        exit()
+except:
+    pass
+
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+if len(BOT_TOKEN) == 0:
+    error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
+
+bot_id = int(BOT_TOKEN.split(':', 1)[0])
+
+DATABASE_URL = environ.get('DATABASE_URL', '')
+if len(DATABASE_URL) == 0:
+    DATABASE_URL = None
+
+if DATABASE_URL:
+    conn = MongoClient(DATABASE_URL)
+    db = conn.mltb
+    if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
+        environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
+        environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
+    conn.close()
+
+load_dotenv('config.env', override=True)
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
+if len(UPSTREAM_REPO) == 0:
+    UPSTREAM_REPO = 'https://github.com/junedkh/jmdkh-mltb'
+
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
+if len(UPSTREAM_BRANCH) == 0:
+    UPSTREAM_BRANCH = 'master'
+
+if path.exists('.git'):
+    run(["rm", "-rf", ".git"])
+
+update = run([f"git init -q \
+                    && git config --global user.email kjuned007@gmail.com \
+                    && git config --global user.name junedkh \
+                    && git add . \
+                    && git commit -sm update -q \
+                    && git remote add origin {UPSTREAM_REPO} \
+                    && git fetch origin -q \
+                    && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+if update.returncode == 0:
+    info('Successfully updated with latest commit from UPSTREAM_REPO')
+else:
+    error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
 
 if not path.isfile('.jmdkh'):
     try:
